d986ecd56e7fac3f14295692061f2b43cebee3c3
arnavgunjikar <81632011+arnavgunjikar@users.noreply.github.com>
2021-12-22 17:48:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function page_1()
+{
+    window.location = "page__1.html";
+}
+
+function page_2()
+{
+    window.location = "page__2.html";
+}
+
+function page_3()
+{
+    window.location = "page__3.html";
+}
+
+function page_4()
+{
+    window.location = "page__4.html";
+}
+
+function page_5()
+{
+    window.location = "page__5.html";
+}
